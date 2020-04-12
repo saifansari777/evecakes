9c80289d189e8685a142bf59cc467f04de401100
Saif Ansari <saifansari@tutanota.com>
2020-04-12 16:30:08 +0530

    static stable 3

--- evecakes/settings.py
+++ evecakes/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+ SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY ='2097ad34337f2d24da45aa90f33714dba2ed9d11db294417'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evecakes.herokuapp.com']
 
 
 # Application definition
